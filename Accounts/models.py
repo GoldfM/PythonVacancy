e3004ccd141ddd5edac0dc25ef92a4a6d0fb648e
Михаил Гольдфарб <goldfarbm@mail.ru>
2024-01-19 21:37:37 +0500

    Добавил графики

--- Accounts/models.py
+++ Accounts/models.py
@@ -5,203 +5,37 @@ from django.template.defaultfilters import slugify
 from django.contrib.auth.models import AbstractUser
 
 from django.contrib.auth import get_user_model
-def user_directory_path(instance, filename):
-    return 'accounts/{0}/{1}'.format(instance.slug, 'avatar')
-
-class User(AbstractUser):
-    sur_sur_name = models.CharField(max_length=20, verbose_name="Фамилия",blank=True)
-    slug = models.SlugField(verbose_name='Слаг', unique=True)
-    descriptions = models.TextField(max_length=300, verbose_name='Описание',blank=True)
-    email = models.EmailField(verbose_name='Почта',blank=True)
-    spec = models.ForeignKey('Specs', db_index=True, on_delete=models.PROTECT, verbose_name="Специализация",blank=True, default=1)
-    photo = models.ImageField(upload_to=user_directory_path, blank=True, verbose_name="Фото")
-    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
-    vk_url = models.URLField(max_length=128, blank=True, verbose_name='VK')
-    hh_url = models.URLField(max_length=128,blank=True, verbose_name='HH')
-    behance_url = models.URLField(max_length=128,blank=True,verbose_name='BH')
-
-    def __str__(self):
-        return self.slug
-
-    def save(self, *args, **kwargs):
-        # Slugify (Cyrillic)
-        alphabet = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z',
-                    'и': 'i',
-                    'й': 'j', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't',
-                    'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ы': 'i', 'э': 'e',
-                    'ю': 'yu',
-                    'я': 'ya'}
-
-        self.slug = slugify(''.join(alphabet.get(w, w) for w in self.username.lower()))
-        super(User, self).save(*args, **kwargs)
-
-
-
-
-    def get_projects(self):
-        return Vacancy.objects.all().filter(users=self)
-
-    def count_projects(self):
-        return self.get_projects().count()
-
-    def get_followers(self):
-        return Follow.objects.all().filter(follow_from_id=self.id).values_list('follow_to', flat=True)
-
-    def count_followers(self):
-        return self.get_followers().count()
-
-    def get_followings(self):
-        return Follow.objects.all().filter(follow_to_id=self.id).values_list('follow_from', flat=True)
-
-    def count_followings(self):
-        return Follow.objects.all().filter(follow_to_id=self.id).count()
-
-    def get_absolute_url(self):
-        return reverse('profile', kwargs={'slug': self.slug})
-
-    def get_short_name(self):
-        return '{0} {1}'.format(self.last_name, self.first_name)
-
-    class Meta:
-        verbose_name = 'Юзер'
-        verbose_name_plural = 'Юзеры'
-        ordering = ['id']
-
-def project_directory_path(instance, filename):
-    return 'projects/{0}/{1}'.format( instance.name, filename)
 
 class Vacancy(models.Model):
     slug = models.SlugField(verbose_name='Слаг', unique=False)
     name = models.CharField(max_length=50, db_index=True, verbose_name="Название")
-    users = models.ManyToManyField(User)
-    type = models.ForeignKey('TypeVacancy', on_delete=models.PROTECT, verbose_name='Тип преокта', default=1)
-    spec_proj = models.ForeignKey('SpecVacancy', on_delete=models.PROTECT, verbose_name='Специализация', default=1)
-    #sum_registred = models.IntegerField(default=0, verbose_name='Сумма оценок зареганных')
-    #count_registred = models.IntegerField(default=0, verbose_name='Количество оценок зареганных')
-    sum_unregistred = models.IntegerField(default=0, verbose_name='Сумма оценок незареганных')
-    count_unregistred = models.IntegerField(default=0, verbose_name='Количество оценок незареганных')
-    descriptions = models.TextField(max_length=300, verbose_name='Описание')
-    key_words= models.TextField(max_length=100, verbose_name='Ключевые слова')
-    url = models.URLField(max_length=128,  blank=True, verbose_name='URL')
-    time_developing = models.CharField(max_length=30,  verbose_name="Время разработки")
-    teammate1 = models.URLField(max_length=128,  blank=True, verbose_name='Тиммейт1')
-    teammate2 = models.URLField(max_length=128,  blank=True, verbose_name='Тиммейт2')
-    teammate3 = models.URLField(max_length=128,  blank=True, verbose_name='Тиммейт3')
-    teammate4 = models.URLField(max_length=128,  blank=True, verbose_name='Тиммейт4')
-    teammate5 = models.URLField(max_length=128,  blank=True, verbose_name='Тиммейт5')
-    avatar_image = models.ImageField(upload_to=project_directory_path, blank=True, verbose_name="Аватар")
-    main_image = models.ImageField(upload_to=project_directory_path, blank=True, verbose_name="Основное фото")
-
-    def teammates(self):
-        return self.users.all()
-    def save(self, *args, **kwargs):
-        # Slugify (Cyrillic)
-        alphabet = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z',
-                    'и': 'i',
-                    'й': 'j', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't',
-                    'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ы': 'i', 'э': 'e',
-                    'ю': 'yu',
-                    'я': 'ya'}
-        self.slug = slugify(''.join(alphabet.get(w, w) for w in self.name.lower()))
-
-        super(Vacancy, self).save(*args, **kwargs)
-        self.users.clear()
-
-        for teammate in [self.teammate1,self.teammate2,self.teammate3,self.teammate4,self.teammate5]:
-            if teammate:
-                self.users.add(User.objects.get(slug = teammate.split('/')[-1]) )
-
-        super(Vacancy, self).save(*args, **kwargs)
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse('project', kwargs={'post_slug': self.slug})
-
-    def get_users(self):
-        return Vacancy.objects.get(slug=self.slug).users.all()
-
-    def get_rate_registred(self):
-        if Rate.objects.filter(project=self).count() > 0:
-            rates = Rate.objects.filter(project=self).values_list('rate', flat=True)
-            return round(sum(rates) / rates.count(), 1)
-        return 0
-
-    def get_users_who_rated(self):
-        return Rate.objects.filter(project=self).values_list('user', flat=True)
-
-    def get_rate_unregistred(self):
-        if self.count_unregistred:
-            return round(self.sum_unregistred / self.count_unregistred, 1)
-        return 0
-
+    salary_from = models.FloatField()
+    salary_to = models.FloatField()
+    salary_currency = models.ForeignKey('Currency')
     class Meta:
         verbose_name = 'Проект'
         verbose_name_plural = 'Проекты'
         ordering = ['id']
 
 
-def image_directory_path(instance, filename):
-    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return 'accounts/projects/{0}/{1}'.format(instance.proj.user.slug, filename)
+class SkillVacancy(models.Model):
+    vacancy = models.ForeignKey('Vacancy', verbose_name='Фото проекта', on_delete=models.CASCADE)
+    skill = models.ForeignKey('Skill', verbose_name='Фото проекта', on_delete=models.CASCADE)
 
-class Image(models.Model):
-    proj = models.ForeignKey('Vacancy', verbose_name='Фото проекта', on_delete=models.CASCADE)
-    photo = models.ImageField(upload_to=image_directory_path)
     class Meta:
         verbose_name = 'Изображение'
         verbose_name_plural = 'Изображения'
         ordering = ['id']
 
-
-class Specs(models.Model):
-    name = models.CharField(max_length=40, verbose_name="Специализация")
+class Currency(models.Model):
+    name = models.CharField(max_length=40)
+    value = models.FloatField()
     def __str__(self):
         return self.name
-
-    class Meta:
-        verbose_name = 'Специализация'
-        verbose_name_plural = 'Специализации'
-        ordering = ['id']
-
-
-class SpecVacancy(models.Model):
-    name = models.CharField(max_length=40, verbose_name="Специализация проекта")
+class Skill(models.Model):
+    name = models.CharField(max_length=40, verbose_name="Навык")
     def __str__(self):
         return self.name
-    class Meta:
-        verbose_name = 'Специализация проекта'
-        verbose_name_plural = 'Специализации проектов'
-        ordering = ['id']
-
-class TypeVacancy(models.Model):
-    type = models.CharField(max_length=40, verbose_name="Тип проекта")
-    def __str__(self):
-        return self.type
-    class Meta:
-        verbose_name = 'Тип'
-        verbose_name_plural = 'Типы'
-        ordering = ['id']
-
-
-class Follow(models.Model):
-    follow_from = models.ForeignKey('User', related_name='follower', on_delete=models.CASCADE, verbose_name='Кто подписался')
-    follow_to = models.ForeignKey('User', related_name='following',on_delete=models.CASCADE, verbose_name='На кого подписался')
-    class Meta:
-        verbose_name = 'Подписка'
-        verbose_name_plural = 'Подписки'
-        ordering = ['id']
-
-class Rate(models.Model):
-    user = models.ForeignKey('User', related_name='user', on_delete=models.CASCADE, verbose_name='Кто оценил')
-    project = models.ForeignKey('Vacancy', related_name='project',on_delete=models.CASCADE, verbose_name='Какой проект оценил')
-    rate = models.IntegerField(default=0, verbose_name='оценка')
-    class Meta:
-        verbose_name = 'Оценка'
-        verbose_name_plural = 'Оценки'
-        ordering = ['id']
-
 
 
 
---
+++ main.py
@@ -0,0 +1,81 @@
+import pandas as pd
+currency_to_rub = {
+    "AZN": 51.5563,
+    "BYR": 27.8408,
+    "EUR": 95.6007,
+    "GEL": 32.9718,
+    "KGS": 98.1295,
+    "KZT": 19.4177,
+    "RUR": 1,
+    "UAH": 23.1474,
+    "USD": 87.6457,
+    "UZS": 0.007135,
+}
+# Загрузка данных из файла CSV в DataFrame
+df = pd.read_csv('vacancies1.csv')
+
+# Преобразование столбца с датой публикации в формат даты
+df['published_at'] = pd.to_datetime(df['published_at'])
+# Создание функции для расчета средней зарплаты с учетом валюты
+def calculate_avg_salary(row, conversion_rates):
+    avg_salary = (row['salary_to'] + row['salary_from']) / 2
+    conversion_rate = conversion_rates[row['salary_currency']]
+    avg_salary_rub = avg_salary * conversion_rate
+    return avg_salary_rub
+
+# Применение функции к каждой строке датафрейма
+df['average_salary'] = df.apply(lambda row: calculate_avg_salary(row, currency_to_rub), axis=1)
+
+# Создание отдельного столбца для года публикации
+df['year_published'] = df['published_at'].dt.year
+
+# Группировка данных по годам и рассчет средней зарплаты и количества вакансий
+average_salary_by_year = df.groupby('year_published')['average_salary'].mean()
+vacancy_count_by_year = df.groupby('year_published').size()
+
+# Визуализация данных
+import matplotlib.pyplot as plt
+
+# График динамики уровня зарплат по годам
+plt.figure(figsize=(10, 6))
+plt.plot(average_salary_by_year.index, average_salary_by_year.values, marker='o')
+plt.title('Динамика уровня зарплат по годам')
+plt.xlabel('Год')
+plt.ylabel('Средняя зарплата')
+
+plt.savefig('salary_year.png')
+
+# График динамики количества вакансий по годам
+plt.figure(figsize=(10, 6))
+plt.plot(vacancy_count_by_year.index, vacancy_count_by_year.values, marker='o', color='r')
+plt.title('Динамика количества вакансий по годам')
+plt.xlabel('Год')
+plt.ylabel('Количество вакансий')
+plt.savefig('count_year.png')
+
+
+
+selected_df = df[df['name'].str.contains("Python") | df['name'].str.contains("python") | df['name'].str.contains("пайтон")]
+
+# Группировка данных по годам и рассчет средней зарплаты и количества вакансий для выбранной профессии
+average_salary_by_year_selected = selected_df.groupby('year_published')['average_salary'].mean()
+vacancy_count_by_year_selected = selected_df.groupby('year_published').size()
+
+# Визуализация динамики уровня зарплат для выбранной профессии по годам
+plt.figure(figsize=(10, 6))
+plt.plot(average_salary_by_year_selected.index, average_salary_by_year_selected.values, marker='o', color='g')
+plt.title('Динамика уровня зарплат для выбранной профессии по годам')
+plt.xlabel('Год')
+plt.ylabel('Средняя зарплата')
+plt.savefig('salary_year_vac.png')
+
+
+# Визуализация динамики количества вакансий для выбранной профессии по годам
+plt.figure(figsize=(10, 6))
+plt.plot(vacancy_count_by_year_selected.index, vacancy_count_by_year_selected.values, marker='o', color='b')
+plt.title('Динамика количества вакансий для выбранной профессии по годам')
+plt.xlabel('Год')
+plt.ylabel('Количество вакансий')
+plt.savefig('count_year_vac.png')
+
+
